00c9ba459e0716a89c54168613cbb4b68b645e45
Mark Wilson <secretchinamark@gmail.com>
2014-11-21 16:59:31 +0800

    Backend changes to support Facebook deceased check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  MINIMUM_RESPONSE_TIME = 1.month
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
